dc51731cc105aaef37abff495a58080aaee46e94
Ignacio Cinalli <nachocinalli@gmail.com>
2024-05-14 11:54:53 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# adapt-GSAPHorizontalScroll
+
+This extension allows you to use the GSAP library to animate elements as you scroll.
+
+Demos: [https://adaptlearning-no-core.web.app/#/id/expo-60](https://adaptlearning-no-core.web.app/#/id/expo-60)
+
+## Dependencies
+[adapt-GSAP](https://github.com/nachocinalli/adapt-GSAP)
---
+++ js/adapt-GSAPHorizontalScroll.js
@@ -0,0 +1,74 @@
+import Adapt from 'core/js/adapt';
+
+class GSAPHorizontalScroll extends Backbone.Controller {
+  initialize() {
+    this.listenTo(Adapt, 'app:dataReady', this.onDataReady);
+  }
+
+  static get courseConfig() {
+    return Adapt.course.get('_GSAPHorizontalScroll');
+  }
+
+  isEnabled() {
+    return !(!GSAPHorizontalScroll.courseConfig || !GSAPHorizontalScroll.courseConfig._isEnabled);
+  }
+
+  onDataReady() {
+    if (!this.isEnabled()) {
+      return;
+    }
+    this.listenTo(Adapt, 'GSAP:ready', this.setupUpEventListeners);
+  }
+
+  setupUpEventListeners() {
+    const modelTypes = ['article', 'block'];
+
+    const modelEventNames = modelTypes.concat(['']).join('View:postRender ');
+
+    this.listenTo(Adapt, modelEventNames, this.onViewRender);
+  }
+
+  onViewRender(view) {
+    const model = view.model;
+    const gsapHorizontalScroll = model.get('_GSAPHorizontalScroll');
+    if (!gsapHorizontalScroll || !gsapHorizontalScroll._isEnabled) {
+      return;
+    }
+    const $selector = view.$el.find(gsapHorizontalScroll.selector);
+    const $horizontalScroll = view.$el.find(gsapHorizontalScroll.horizontalScroll);
+
+    if ($selector.length) {
+      $selector.addClass('has-gsaphorizontalscroll');
+      this.setupHorizontalScroll($selector, $horizontalScroll);
+    }
+  }
+
+  setupHorizontalScroll($view, $el) {
+    const viewEl = $view[0];
+    const el = $el[0];
+
+    const getScrollAmount = () => {
+      const viewWidth = el.scrollWidth;
+
+      return -(viewWidth - window.innerWidth + window.innerWidth / 2);
+    };
+    const tween = Adapt.GSAP.lib.to(el, {
+      x: getScrollAmount,
+      duration: 3,
+      ease: 'none'
+    });
+
+    Adapt.GSAP.ScrollTrigger.create({
+      trigger: viewEl,
+      start: 'top 10%',
+      end: () => `+=${getScrollAmount() * -1}`,
+      pin: true,
+      animation: tween,
+      scrub: 1,
+      invalidateOnRefresh: true,
+      markers: false
+    });
+  }
+}
+
+export default new GSAPHorizontalScroll();
